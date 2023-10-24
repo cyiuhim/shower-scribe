466f30894d560ca8d6d01f790419c2e830ac5eb3
SuperZooper3 <54122151+SuperZooper3@users.noreply.github.com>
2023-10-24 15:23:04 -0400

    add html and flask for recording display

--- webserver/app.py
+++ webserver/app.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
+
+@app.route('/recordings/<path:path>')
+def send_recording(path):
+    return send_from_directory('recordings', path)
+
 
 @app.route('/', methods=["GET", "POST"])
 def show_main():
