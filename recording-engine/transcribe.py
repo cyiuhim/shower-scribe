c21b82e4c3dcd39af5dc83ce5d1ef9d099064191
SuperZooper3 <54122151+SuperZooper3@users.noreply.github.com>
2023-11-07 17:11:00 -0500

    add comments

--- recording-engine/transcribe.py
+++ recording-engine/transcribe.py
@@ -1,6 +1,6 @@
 import assemblyai as aai
-import sounddevice as sd
-from scipy.io.wavfile import write
+# import sounddevice as sd # commenting this out since your code doesn't have to record, it just takes premade files
+# from scipy.io.wavfile import write
 import base64
 import os
 import os.path
@@ -9,15 +9,14 @@ from pathlib import Path
 rate = 48000
 duration = 5
 
-aai.settings.api_key = f"91b2eda6b5b3474289b0649020d9245b"
+aai.settings.api_key = f"91b2eda6b5b3474289b0649020d9245b" # make sure to not hard code environment variables, instead we're using a .env file that'll be managed by the director. for now this is fine but we'll have to reset the key
 
-recordpath = "D:/coding/showerscribe/recordings/test.wav"
+recordpath = "../webserver/userdata/recordings/talking_test.wav" # use relative paths, not absolute paths, and use them through the os module. because this will only work on your machine. it would be something like "../webserver/userdata/recordings/test.wav"
 
-def recording():
-    
-    testrecord = sd.rec(int(rate * duration), samplerate=rate, channels=2)
-    sd.wait()
-    write(recordpath, rate, testrecord)
+# def recording():
+#     testrecord = sd.rec(int(rate * duration), samplerate=rate, channels=2)
+#     sd.wait()
+#     write(recordpath, rate, testrecord)
 
 transcriber = aai.Transcriber()
 transcript = transcriber.transcribe(recordpath) #TODO: INSERT PATH FOR AUDIO FILE 
