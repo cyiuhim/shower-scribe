2a70bbba0418de9831d15827784b0df21a8d86bb
Vincent Chung <yhchung@uwaterloo.ca>
2023-11-14 13:17:56 -0500

    added test recordings

--- recording-engine/transcribe.py
+++ recording-engine/transcribe.py
@@ -1,6 +1,4 @@
 import assemblyai as aai
-# import sounddevice as sd # commenting this out since your code doesn't have to record, it just takes premade files
-# from scipy.io.wavfile import write
 import base64
 import os
 import os.path
@@ -9,7 +7,7 @@ from pathlib import Path
 rate = 48000
 duration = 5
 
-aai.settings.api_key = f"91b2eda6b5b3474289b0649020d9245b" # make sure to not hard code environment variables, instead we're using a .env file that'll be managed by the director. for now this is fine but we'll have to reset the key
+#aai.settings.api_key = f"91b2eda6b5b3474289b0649020d9245b" # make sure to not hard code environment variables, instead we're using a .env file that'll be managed by the director. for now this is fine but we'll have to reset the key
 
 recordpath = "../webserver/userdata/recordings/talking_test.wav" # use relative paths, not absolute paths, and use them through the os module. because this will only work on your machine. it would be something like "../webserver/userdata/recordings/test.wav"
 
