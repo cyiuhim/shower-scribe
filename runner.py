7126003e70b55e43396edab1f22cba53bfa6cd27
Tyler Chen <tyche4490@gmail.com>
2023-11-19 17:26:35 -0500

    added webserver startup to runner

--- runner.py
+++ runner.py
@@ -1,21 +1,21 @@
 from pynput import keyboard 
 from multiprocessing import Process
 from workers.recorder import Recorder
+from webserver.app import startup_webserver
 
 from datetime import datetime
 import sys
 import os
 
+
 def listen_for_input():
-    is_listening = False
     recorder = Recorder(stereo=False)
     while True:
         with keyboard.Events() as events:
             for event in events:
-                if event.key == keyboard.Key.space and not is_listening:
+                if event.key == keyboard.Key.space and not recorder.is_recording:
                     recorder.start_recording()
-                    is_listening = True
-                if event.key == keyboard.Key.shift_l and is_listening:
+                if event.key == keyboard.Key.shift_l and recorder.is_recording:
                     status = recorder.save_recording("./webserver/userdata/recordings", datetime.now().strftime("recording-%Y-%m-%d-%H-%m-%s.wav"))
                     print(status)
                     is_listening = False
@@ -23,5 +23,8 @@ def listen_for_input():
 if __name__ == "__main__":
     p = Process(target=listen_for_input)
     p.start()
+    flask_server = Process(target=startup_webserver)
+    flask_server.start()
     p.join()
+    flask_server.join()
 
