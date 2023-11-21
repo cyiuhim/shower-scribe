7178cef1765b1da75a81728535a312cac8456210
Alex Aumais <alex.aumais@uwaterloo.ca>
2023-11-21 14:20:28 -0500

    Abstracted External Database Interfaces and Cohere Resume Methods

---
+++ sql_interface.py
@@ -0,0 +1,106 @@
+# A module that handles all database interactions for the SQL database for external modules, 
+# The slightly odd structure of this though is that the database is still owned by the flask server to keep all of the model interractions simple
+
+from webserver.app import app, db, Recording, TextFile
+
+# Database methods
+
+def get_recording_path(recording_id):
+    with app.app_context():
+        recording = Recording.query.filter_by(id=recording_id).first()
+        if recording:
+            return recording.recording_filename
+        else:
+            return None
+        
+def get_recording_as_dict(recording_id):
+    with app.app_context():
+        recording = Recording.query.filter_by(id=recording_id).first()
+        if recording:
+            return {
+                "id": recording.id,
+                "recording_filename": recording.recording_filename,
+                "display_name": recording.display_name,
+                "associated_transcription_id": recording.associated_transcription_id,
+                "associated_resume_id": recording.associated_resume_id,
+                "flag_transcribed": recording.flag_transcribed,
+                "flag_resumed": recording.flag_resumed,
+            }
+        else:
+            return None
+        
+def get_text_as_dict(text_id):
+    with app.app_context():
+        text = TextFile.query.filter_by(id=text_id).first()
+        if text:
+            return {
+                "id": text.id,
+                "text_filename": text.text_filename,
+                "display_name": text.display_name,
+                "type": text.type,
+                "associated_recording_id": text.associated_recording_id
+            }
+        else:
+            return None
+        
+
+def create_text_from_dict(text_dict):
+    # check that there are the right feilds
+    needed_keys = ["text_filename","display_name","type","associated_recording_id"]
+    for key in needed_keys:
+        if key not in text_dict:
+            raise ValueError(f"Error, text_dict missing key {key}")
+    try:
+        with app.app_context():
+            text = TextFile()
+            text.text_filename = text_dict["text_filename"]
+            text.display_name = text_dict["display_name"]
+            text.type = text_dict["type"]
+            text.associated_recording_id = text_dict["associated_recording_id"]
+            db.session.add(text)
+            db.session.commit()
+            return text.id
+    except:
+        return None
+
+def update_recording_associated_resume(recording_id,new_resume_id):
+    # update the associated resume for a recording
+    with app.app_context():
+        recording = Recording.query.filter_by(id=recording_id).first()
+        if recording:
+            recording.associated_resume_id = new_resume_id
+            db.session.commit()
+            return True
+        else:
+            return False
+        
+def update_recording_display_name(recording_id,new_display_name):
+    # update the display name for a recording
+    with app.app_context():
+        recording = Recording.query.filter_by(id=recording_id).first()
+        if recording:
+            recording.display_name = new_display_name
+            db.session.commit()
+            return True
+        else:
+            return False
+        
+def update_recording_flag_transcribed(recording_id):
+    with app.app_context():
+        recording = Recording.query.filter_by(id=recording_id).first()
+        if recording:
+            recording.flag_transcribed = True
+            db.session.commit()
+            return True
+        else:
+            return False
+        
+def update_recording_flag_resumed(recording_id):
+    with app.app_context():
+        recording = Recording.query.filter_by(id=recording_id).first()
+        if recording:
+            recording.flag_resumed = True
+            db.session.commit()
+            return True
+        else:
+            return False
