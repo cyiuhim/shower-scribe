b1083147bd669e71e06df459d8391ef09a57fe68
Balaji Leninrajan <bleninrajan@uwaterloo.ca>
2023-11-25 21:24:32 -0500

    Added GPIO LED

--- webserver/reset_database.py
+++ webserver/reset_database.py
@@ -1,7 +1,7 @@
 # THIS FILE SHOULD ONLY BE RUN BY HAND
 
 if __name__ == "__main__":
-    from webserver.app import *
+    from app import *
     with app.app_context():
         db.drop_all()
         db.create_all()
