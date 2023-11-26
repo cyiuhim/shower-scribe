929778d89ef74aed1cd14544055bfde844dfcd84
Balaji Leninrajan <bleninrajan@uwaterloo.ca>
2023-11-25 23:03:58 -0500

    Implemented deleting

--- webserver/app.py
+++ webserver/app.py
@@ -22,21 +22,23 @@ from flask import request, jsonify
 app = Flask(__name__)
 
 default_settings = {
-    "clustering_time_minutes" : 180,
-    "transcription":True,
-    "resume":True,
+    "clustering_time_minutes": 180,
+    "transcription": True,
+    "resume": True,
 }
 
 # Settings setup
 # This goes here instead of the data_interface because it's used by the flask app, not the data_interface, and this would lead to circular imports
 current_dir = os.path.dirname(os.path.realpath(__file__))
-user_settings_path = os.path.join(current_dir,'userdata','user_settings.json')
+user_settings_path = os.path.join(
+    current_dir, 'userdata', 'user_settings.json')
 user_settings = {}
 if os.path.exists(user_settings_path):
     with open(user_settings_path, 'r') as f:
         user_settings = json.load(f)
 else:
-    print(f"No settings file found {user_settings_path}. Using default settings.")
+    print(
+        f"No settings file found {user_settings_path}. Using default settings.")
 
     user_settings = default_settings
     try:
@@ -53,7 +55,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-
 # Defined database models
 
 class Recording(db.Model):
@@ -62,26 +63,29 @@ class Recording(db.Model):
                            server_default=func.now())
     recording_filename = db.Column(db.Text)
     display_name = db.Column(db.Text)
-    associated_transcription_id = db.Column(db.Integer, db.ForeignKey('text_file.id'))
+    associated_transcription_id = db.Column(
+        db.Integer, db.ForeignKey('text_file.id'))
     associated_resume_id = db.Column(db.Integer, db.ForeignKey('text_file.id'))
-    flag_transcribed = db.Column(db.Boolean,default=False)
-    flag_resumed = db.Column(db.Boolean,default=False)
+    flag_transcribed = db.Column(db.Boolean, default=False)
+    flag_resumed = db.Column(db.Boolean, default=False)
 
     def __repr__(self):
         return f'<Recording {self.id} ({self.recording_filename})>'
-    
+
+
 class TextFile(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     created_at = db.Column(db.DateTime(timezone=True),
                            server_default=func.now())
-    type = db.Column(db.Integer) # 0 = transcription, 1 = resume, 2 = brainstorm
+    # 0 = transcription, 1 = resume, 2 = brainstorm
+    type = db.Column(db.Integer)
     text_filename = db.Column(db.Text)
     display_name = db.Column(db.Text)
-    associated_recording_id = db.Column(db.Integer, db.ForeignKey('recording.id'))
+    associated_recording_id = db.Column(
+        db.Integer, db.ForeignKey('recording.id'))
 
     def __repr__(self):
         return f'<TextFile {self.id} ({self.text_filename} type {self.type})>'
-    
 
 
 # Routes
@@ -113,6 +117,7 @@ def recordings_on_date(date):
 def send_recording(path):
     return send_from_directory('userdata/recordings', path)
 
+
 @app.route('/recordings/<int:recording_id>', methods=["GET", "POST"])
 def show_recording(recording_id):
     recording = Recording.query.get_or_404(recording_id)
@@ -137,32 +142,57 @@ def show_text(text_id):
     text = TextFile.query.get_or_404(text_id)
     # safely get the content of the text file
     try:
-        with open(os.path.join('webserver','userdata','texts',text.text_filename), "r") as f:
+        with open(os.path.join('webserver', 'userdata', 'texts', text.text_filename), "r") as f:
             content = f.read()
     except:
         content = "Error reading text file."
-    return render_template('text.html', text=text, content=content) # we pass the text without any escaping
- 
-@app.route('/delete_recording/<int:recording_id>', methods=['POST']) #in progress. go through and kill all the children
+    # we pass the text without any escaping
+    return render_template('text.html', text=text, content=content)
+
+
+# in progress. go through and kill all the children
+@app.route('/delete_recording/<int:recording_id>', methods=['POST'])
 def delete_recording(recording_id):
     recording = Recording.query.get(recording_id)
     if recording:
+        recording_path: str = os.path.join(
+            "webserver", "userdata", "recordings", recording.recording_filename)
+
+        transcription = TextFile.query.get(
+            recording.associated_transcription_id)
+        if transcription:
+            transcription_path: str = os.path.join(
+                "webserver", "userdata", "texts", transcription.text_filename)
+            os.remove(transcription_path)
+            db.session.delete(transcription)
+
+        resume = TextFile.query.get(recording.associated_resume_id)
+        if resume:
+            resume_path: str = os.path.join(
+                "webserver", "userdata", "texts", resume.text_filename)
+            os.remove(resume_path)
+            db.session.delete(resume)
+
+        os.remove(recording_path)
         db.session.delete(recording)
         db.session.commit()
         return jsonify({"success": True})
     else:
         return jsonify({"success": False}), 404
-    
+
+
 @app.route('/settings', methods=['GET'])
 def show_settings():
     basedir = os.path.abspath(os.path.dirname(__file__))
-    user_settings_path = os.path.join(basedir, 'userdata','user_settings.json')
+    user_settings_path = os.path.join(
+        basedir, 'userdata', 'user_settings.json')
     user_settings = {}
     if os.path.exists(user_settings_path):
         with open(user_settings_path, 'r') as f:
             user_settings = json.load(f)
     else:
-        print(f"No settings file found {user_settings_path}. Using default settings.")
+        print(
+            f"No settings file found {user_settings_path}. Using default settings.")
         # Define default settings here if needed
         user_settings = default_settings
         try:
@@ -170,19 +200,24 @@ def show_settings():
                 json.dump(user_settings, f)
         except:
             print(f"Error saving settings file {user_settings_path}")
-    
+
     return render_template('settings.html', user_settings=user_settings)
+
 
 @app.route('/settings', methods=['POST'])
 def save_settings():
     new_settings = request.form.to_dict()
 
-    user_settings["clustering_time_minutes"] = int(new_settings.get("grouping_minutes",default_settings["clustering_time_minutes"]))
-    user_settings["transcription"] = new_settings.get("transcription_switch",'off') == 'on'
-    user_settings["resume"] = new_settings.get("llm_switch",'off') == 'on' # the switches only reutnr values if they're true for some reason
-    
+    user_settings["clustering_time_minutes"] = int(new_settings.get(
+        "grouping_minutes", default_settings["clustering_time_minutes"]))
+    user_settings["transcription"] = new_settings.get(
+        "transcription_switch", 'off') == 'on'
+    # the switches only reutnr values if they're true for some reason
+    user_settings["resume"] = new_settings.get("llm_switch", 'off') == 'on'
+
     basedir = os.path.abspath(os.path.dirname(__file__))
-    user_settings_path = os.path.join(basedir, 'userdata','user_settings.json')
+    user_settings_path = os.path.join(
+        basedir, 'userdata', 'user_settings.json')
     try:
         with open(user_settings_path, 'w') as f:
             json.dump(user_settings, f)
@@ -190,15 +225,18 @@ def save_settings():
         print(f"Error saving settings file {user_settings_path}")
     return redirect(url_for('show_main'))
 
+
 @app.route('/search')
 def show_search():
     # This function simply renders the search.html template.
     # You can pass additional context or settings if required.
     return render_template('search.html')
 
+
 @app.route('/search_results')
 def search_results():
-    query = request.args.get('query', '')  # Get the search query from URL parameters
+    # Get the search query from URL parameters
+    query = request.args.get('query', '')
 
     # Assuming get_n_closest_ids function returns a list of result IDs
     result_ids = get_n_closest_ids(1, query, 3)
@@ -209,30 +247,36 @@ def search_results():
     # Render a template with the search results and the original query
     return render_template('search_results.html', query=query, results=results)
 
+
 @app.route('/everything')
 def show_everything():
     # Fetch and group the recordings by date
-    all_recordings = Recording.query.order_by(Recording.created_at.desc()).all()
+    all_recordings = Recording.query.order_by(
+        Recording.created_at.desc()).all()
     all_texts = TextFile.query.order_by(TextFile.created_at.desc()).all()
 
     # No need to convert dates here, as they are already formatted as strings in get_grouped_recordings
     return render_template('everything.html', all_recordings=all_recordings, all_texts=all_texts)
 
-#functions
+# functions
+
 
 def get_grouped_recordings():
-    recordings = Recording.query.order_by(Recording.created_at.desc()).all()  # Sort by created_at in descending order
+    # Sort by created_at in descending order
+    recordings = Recording.query.order_by(Recording.created_at.desc()).all()
     grouped_recordings = defaultdict(list)
     for recording in recordings:
         date_key = recording.created_at.strftime('%B %d, %Y')
         grouped_recordings[date_key].append(recording)
     return dict(grouped_recordings)
 
+
 def group_recordings(recordings):
     recordings.sort(key=lambda r: r.created_at)
     grouped_sessions = []
     session = []
-    clustering_minutes = user_settings["clustering_time_minutes"]  # Directly use the setting
+    # Directly use the setting
+    clustering_minutes = user_settings["clustering_time_minutes"]
 
     for recording in recordings:
         if not session or recording.created_at - session[0].created_at <= timedelta(minutes=clustering_minutes):
@@ -247,12 +291,14 @@ def group_recordings(recordings):
     # Assign session titles
     for session in grouped_sessions:
         first_recording_time = session[0].created_at
-        if first_recording_time.hour < 11 or (first_recording_time.hour == 11 and first_recording_time.minute <= 30): # Morning session if before 11:30
+        # Morning session if before 11:30
+        if first_recording_time.hour < 11 or (first_recording_time.hour == 11 and first_recording_time.minute <= 30):
             session_title = "Morning Session"
-        elif first_recording_time.hour < 17 or (first_recording_time.hour == 17 and first_recording_time.minute <= 30): # Afternoon session if before 17:30 (5:30pm)
+        # Afternoon session if before 17:30 (5:30pm)
+        elif first_recording_time.hour < 17 or (first_recording_time.hour == 17 and first_recording_time.minute <= 30):
             session_title = "Afternoon Session"
         else:
-            session_title = "Evening Session" # Otherwise it's an evening session
+            session_title = "Evening Session"  # Otherwise it's an evening session
 
         for recording in session:
             recording.session_title = session_title
@@ -260,12 +306,11 @@ def group_recordings(recordings):
     return grouped_sessions
 
 
-
 # Run the app
 
 def startup_webserver(debug=False):
-    app.run(host='0.0.0.0', debug=debug,port=8000)
+    app.run(host='0.0.0.0', debug=debug, port=8000)
+
 
 if __name__ == "__main__":
     startup_webserver(debug=True)
-    
