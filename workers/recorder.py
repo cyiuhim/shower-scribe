702bbe2e29e1d66f5c406eac09ad26aa86f1314b
Tyler Chen <tyche4490@gmail.com>
2023-11-25 21:20:38 -0500

    REMOVED A SINGLE PERIODgit add .git add . WOWgit add .!

--- workers/recorder.py
+++ workers/recorder.py
@@ -102,7 +102,7 @@ class Recorder:
         self.recorder_thread.join()  # waits for thread to finish by itself
 
         # creates a path to a file in the form ../$directory/$filename
-        path_home: str = "../"
+        path_home: str = "./"
         file_path: str = path.join(path_home, directory, filename)
 
         # configuring the file
